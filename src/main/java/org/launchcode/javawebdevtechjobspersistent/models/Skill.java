5a04db977f1d340a697c81b8f823d2e329264771
Tyler <ketsugo.h@gmail.com>
2021-03-25 21:41:13 -0500

    Big commit because I got carried away: Set up SQL db, created controllers/repositories for employer, skill classes - functional add skill and add employer features.

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,35 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+
+    @NotNull
+    private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private List<Job> jobs;
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 }
